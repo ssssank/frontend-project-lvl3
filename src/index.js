feb0e9f9b974a204bfa0f36325c432103c1c2840
Alex Litvinov <ssssank@gmail.com>
2020-04-21 23:20:32 +0300

    Add bootstrap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,1 @@
-// import 'bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-
-import init from './init';
-
-init();
+import 'bootstrap/dist/css/bootstrap.min.css';
--- src/init.js
+++
@@ -1,9 +0,0 @@
-// @ts-check
-
-import Example from './Example';
-
-export default () => {
-  const element = document.getElementById('container');
-  const obj = new Example(element);
-  obj.init();
-};
