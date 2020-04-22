159e7bddd64d8d384ccc587688f90da070806e38
Alex Litvinov <ssssank@gmail.com>
2020-04-22 22:30:06 +0300

    small step

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+/* eslint-env browser */
+
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+const form = document.querySelector('form');
+const output = document.querySelector('.output');
+
+form.elements.inputRSS.addEventListener('input', (e) => {
+  output.innerHTML = e.target.value;
+});
